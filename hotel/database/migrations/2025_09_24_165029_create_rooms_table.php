69cd11727ac58ce265479f48cb6eec0768f77b3c
iodza <iodzamonkmode@gmail.com>
2025-09-24 13:10:32 -0400

    Co-authored-by: Mullabaev Nazimzhon <vascreat@users.noreply.github.com>

---
+++ hotel/database/migrations/2025_09_24_165029_create_rooms_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("room_nb");
+            $table->string("room_type");
+            $table->foreignId("hotel_id")->references("id")->on("hotels")->onUpdate("cascade")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
