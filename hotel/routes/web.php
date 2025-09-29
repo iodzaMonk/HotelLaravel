bd3dbb17ddef0c71d28045007b84cc987f720862
Nazim_Maison <nazim071305@gmail.com>
2025-09-29 19:21:04 -0400

    [Testing] Ajout de la page hotel dans les routes et une affichage des hotels disponibles dans la base de donner

--- hotel/routes/web.php
+++ hotel/routes/web.php
@@ -4,12 +4,16 @@ use App\Http\Controllers\HotelController;
 use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
-// Route::resources([
-//     'hotels' => HotelController::class,
-//     'rooms' => RoomController::class,
-// ]);
 
 
 Route::get('/', function () {
     return view('hub');
 });
+
+Route::get('/hotels', [HotelController::class, 'index'])->name('hotels.index');
+
+
+Route::resources([
+    'hotels' => HotelController::class,
+    'rooms' => RoomController::class,
+]);
